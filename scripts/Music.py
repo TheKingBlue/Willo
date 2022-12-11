fb409720de49aea155cd11046640b6b78ed01931
Matthias <matthiasnijman@hotmail.com>
2022-12-11 12:41:33 +0100

    Cleaned up backend

--- scripts/Music.py
+++ scripts/Music.py
@@ -8,91 +8,104 @@ class Player():
         self.queue = []
         self.names = []
     
-    def sing(self, voice, url):
-        voice.sing(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url)), after= lambda error: self.bot.loop.create_task(self.next(voice)))
+    def play(self, voice, url):
+        voice.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url)), after= lambda error: self.bot.loop.create_task(self.next(voice)))
 
     async def next(self, voice):
         if len(self.queue) > 0:
             voice.stop()
-            self.sing(voice, self.queue[0])
+            self.play(voice, self.queue[0])
             self.queue.pop(0)
             self.names.pop(0)
-    
-    async def play(self, message, bot):
-        content = message.content[11:]
-        try:
-            voiceChannel = discord.utils.get(message.author.guild.voice_channels, name=message.author.voice.channel.name)
-            await voiceChannel.connect()
-            voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
 
-        except:
-            voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
-            if voice == None:
-                await message.channel.send('Something went wrong. Are you even in a channel?')
-                return
-            else:
-                pass
-        
-        if message.content == ('willo play') or message.content == ('willo play '):
+
+async def play(message, bot, musicbot):
+    content = message.content[11:]
+    try:
+        voiceChannel = discord.utils.get(message.author.guild.voice_channels, name=message.author.voice.channel.name)
+        await voiceChannel.connect()
+        voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
+
+    except:
+        voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
+        if voice == None:
+            await message.channel.send('Something went wrong. Are you even in a channel?')
             return
-        elif voice.is_playing():
-            try:
-                if not ("youtube.com/watch?" in content or "https://youtu.be/" in content):
-                    info = bot.loop.run_in_executor(None, lambda: youtube_dl.YoutubeDL({"format" : "bestaudio", "quiet": True}).extract_info(f"ytsearch1:{content}", download=False, ie_key="YoutubeSearch"))
-                    yturl = [entry["webpage_url"] for entry in info["entries"]]    
-                else:
-                    yturl = [content]
-                    
-                url = pafy.new(yturl[0]).getbestaudio().url
-                await message.channel.send(f"Added {yturl[0]} to queue!")
-                self.queue += [url]
-                self.names += [yturl]
-            except:
-                return await message.channel.send("Not so fast!")
-                
         else:
-            try:
-                if not ("youtube.com/watch?" in content or "https://youtu.be/" in content):
-                    info = await bot.loop.run_in_executor(None, lambda: youtube_dl.YoutubeDL({"format" : "bestaudio", "quiet": True}).extract_info(f"ytsearch1:{content}", download=False, ie_key="YoutubeSearch"))
-                    yturl = [entry["webpage_url"] for entry in info["entries"]]            
-                else:
-                    yturl = [content]
-                    
-                url = pafy.new(yturl[0]).getbestaudio().url
-                await message.channel.send(f"Now playing: {yturl[0]}")
-                self.sing(voice, url)
-            except:
-                return await message.channel.send("Not so fast!")
+            pass
     
-    async def leave(self, message):
-        voice = discord.utils.get(self.bot.voice_clients, guild=message.author.guild)
-        self.queue = []
-        self.names = []
+    if message.content == ('willo play') or message.content == ('willo play '):
+        return
+    elif voice.is_playing():
         try:
-            await message.channel.send("Disconnecting, give me a second to clean up my record player!")
-            return await voice.disconnect()
+            if not ("youtube.com/watch?" in content or "https://youtu.be/" in content):
+                info = await bot.loop.run_in_executor(None, lambda: youtube_dl.YoutubeDL({"format" : "bestaudio", "quiet": True}).extract_info(f"ytsearch1:{content}", download=False, ie_key="YoutubeSearch"))
+                yturl = [entry["webpage_url"] for entry in info["entries"]]    
+            else:
+                yturl = [content]
+                
+            url = pafy.new(yturl[0]).getbestaudio().url
+            await message.channel.send(f"Added {yturl[0]} to queue!")
+            musicbot.queue += [url]
+            musicbot.names += [yturl]
         except:
-            return await message.channel.send("I'm not even connected. Damn.")
+            return await message.channel.send("Not so fast!")
+            
+    else:
+        try:
+            if not ("youtube.com/watch?" in content or "https://youtu.be/" in content):
+                info = await bot.loop.run_in_executor(None, lambda: youtube_dl.YoutubeDL({"format" : "bestaudio", "quiet": True}).extract_info(f"ytsearch1:{content}", download=False, ie_key="YoutubeSearch"))
+                yturl = [entry["webpage_url"] for entry in info["entries"]]            
+            else:
+                yturl = [content]
+                
+            url = pafy.new(yturl[0]).getbestaudio().url
+            await message.channel.send(f"Now playing: {yturl[0]}")
+            musicbot.play(voice, url)
+        except:
+            return await message.channel.send("Not so fast!")
+
+async def leave(message, bot, musicbot):
+    voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
+    musicbot.queue = []
+    musicbot.names = []
+    try:
+        await message.channel.send("Disconnecting, give me a second to clean up my record player!")
+        return await voice.disconnect()
+    except:
+        return await message.channel.send("I'm not even connected. Damn.")
     
-    async def pause(self, message):
-        voice = discord.utils.get(self.bot.voice_clients, guild=message.author.guild)
+async def pause(message, bot):
+        voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
         try:
             return await voice.pause()
         except:
             if voice == None:
                 await message.channel.send("I wasn't even speaking. What is it with you people? ...")
     
-    async def resume(self, message):
-        voice = discord.utils.get(self.bot.voice_clients, guild=message.author.guild)
+async def resume(message, bot):
+        voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
         try:
             return await voice.resume()
         except:
             if voice == None:
                 await message.channel.send("I'm not even there!")
-
-    async def skip(self, message):
+    
+async def queue(message, musicbot):
+        if len(musicbot.queue) == 0:
+            await message.channel.send("There is no queue.")
+        else:
+            embed = discord.Embed(title="The Queue", description="", colour=discord.Colour.blurple())
+            i = 1
+            for name in musicbot.names:
+                embed.description += f"{i} - {name[0]}\n"
+                i += 1
+            embed.set_footer(text="The queue can be cleared with 'Willo leave'")
+            await message.channel.send(embed=embed)
+    
+async def skip(message, bot, musicbot):
         voiceChannel = discord.utils.get(message.author.guild.voice_channels, name=message.author.voice.channel.name)
-        voice = discord.utils.get(self.bot.voice_clients, guild=message.author.guild)
+        voice = discord.utils.get(bot.voice_clients, guild=message.author.guild)
         if voice == None:
             return await message.channel.send("I am not playing a song right now")
         if voiceChannel == None:
@@ -102,16 +115,4 @@ class Player():
         else:
             voice.stop()
             await message.channel.send("Skipping!")
-            self.next(voice)
-
-    async def queue(self, message):
-        if len(self.queue) == 0:
-            await message.channel.send("There is no queue.")
-        else:
-            embed = discord.Embed(title="The Queue", description="", colour=discord.Colour.blurple())
-            i = 1
-            for name in self.names:
-                embed.description += f"{i} - {name[0]}\n"
-                i += 1
-            embed.set_footer(text="The queue can be cleared with 'Willo leave'")
-            await message.channel.send(embed=embed)
+            await musicbot.next(voice)
--- scripts/Responses.py
+++ scripts/Responses.py
@@ -2,6 +2,7 @@ import random
 
 #global variables
 GREETING = ['Hallo', 'Hey', 'Goededag', 'Sup', 'Heyo']
+COMMANDS = 'Commands, Decide on, Willo play, Willo skip, Willo pause, Willo continue, Willo queue, Willo leave'
 
 async def decide(message):
     content = message.content[9:]
@@ -58,21 +59,45 @@ async def greet(message):
         await message.channel.send(f"{greet} {naam}!")
     
 async def willo(message):
-        content = message.content.lower()
-        if content == 'willo' or content == 'willo?' or content == 'willo ':
-            await message.channel.send('Ja?')
-        elif message.content.lower().startswith(('willo play', 'willo pause', 'willo continue', 'willo stop', 'willo leave', 'willo queue', 'willo skip')):
-            pass
-        elif 'matthias' in content:
-            await message.channel.send("Matthias is my creator, I am forever thankfull! :D")
-        elif 'nederlan' in content or 'engel' in content or 'dutch' in content or 'english' in content or 'taal' in content or 'language' in content:
-            await message.channel.send('Ik ben tweetalig :D. Ik spreek Nederlands en Engels.')
-        elif 'kan j' in content or 'doe j' in content or 'werk j' in content or 'do you' in content:
-            if message.author.id == 254534396390932491:
-                await message.channel.send('Ik kan: Commands, Decide on, Summon, Willo play, Willo skip, Willo pause, Willo continue, Willo queue, Willo leave')
-            else:
-                await message.channel.send('Ik kan alles.')
-        elif 'introduce' in content:
-            pass
+    content = message.content.lower()
+    if content == 'willo' or content == 'willo?' or content == 'willo ':
+        await message.channel.send('Ja?')
+    elif message.content.lower().startswith(('willo play', 'willo pause', 'willo continue', 'willo stop', 'willo leave', 'willo queue', 'willo skip')):
+        pass
+    elif 'matthias' in content:
+        await message.channel.send("Matthias is my creator, I am forever thankfull! :D")
+    elif 'nederlan' in content or 'engel' in content or 'dutch' in content or 'english' in content or 'taal' in content or 'language' in content:
+        await message.channel.send('Ik ben tweetalig :D. Ik spreek Nederlands en Engels.')
+    elif 'kan j' in content or 'doe j' in content or 'werk j' in content or 'do you' in content:
+        if message.author.id == 254534396390932491:
+            await message.channel.send('Ik kan: Commands, Decide on, Summon, Willo play, Willo skip, Willo pause, Willo continue, Willo queue, Willo leave')
         else:
-            await message.channel.send('Stoor mij niet met zulke onnozele kwesties.')
+            await message.channel.send('Ik kan alles.')
+    elif 'introduce' in content:
+        pass
+    else:
+        await message.channel.send('Stoor mij niet met zulke onnozele kwesties.')
+
+async def introduce(message):
+    if message.author.id == 254534396390932491:
+            await message.channel.send(
+                "@everyone **My name is willo!**\n"
+                "I am a discord bot created by <@254534396390932491>. I have been designed to be a member of the Ludit server more than a bot.\n"
+                "However I do have some bot like functions such as playing music! :D Therefore I have also been deployed in other servers.\n"
+                "I do not work with a preset, just speak to me with my name and I will respond! (Maybe not always)\n"
+                "As of right now I am only online when my maker has my script running. Not to worry tho, as a 24/7 uptime is in production.\n"
+                "Anywway, I hope you will enjoy my presence. If you have any questions or suggestions, ask <@254534396390932491>!\n"
+                "This is my first launch, I will keep you posted if I ever get updated!\n"
+                "*V1.4*")
+
+async def commands(message):
+    if message.author.id == 254534396390932491:
+        await message.channel.send(COMMANDS)
+    else:
+        await message.channel.send('Jij bent mijn baas niet. I am a strong and independant bot. Slay.')
+    
+async def help(message):
+    if message.author.id == 254534396390932491:
+        await message.channel.send(COMMANDS)
+    else:
+        await message.channel.send('Het is te laat om jou te helpen.')
